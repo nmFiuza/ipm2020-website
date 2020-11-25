c77c04ea6cea4878e3e427fd11d195f84f47fae4
Hugo Santiago <h.assuncao@campus.fct.unl.pt>
2020-11-25 00:53:27 +0000

    random livro

--- assets/js/mistery.js
+++ assets/js/mistery.js
@@ -1,4 +1,50 @@
+const cheapOptionConst = 200;
+const midOptionConst = 300;
+const expensiveOptionConst = 400;
+
 var logged = getLogged();
+var userPoints = logged.points;
 var points = document.getElementById("ecoPoints");
-points.innerHTML = logged.points;
+points.innerHTML = userPoints;
 var userBooks = getUserBooks(logged.id);
+
+function cheapOption(){
+    if(userPoints<cheapOptionConst){
+        window.alert("Pontos insuficientes, necessita mais " + (cheapOptionConst-userPoints) + " pontos");
+        return;
+    }
+    if(confirm("Tem a certeza que pretende gastar " + cheapOptionConst + " pontos?")){
+        removePointsFromUser(cheapOptionConst);
+        window.location.href = "random_mistery.html";
+    }
+}
+
+function midOption(){
+    if(userPoints<midOptionConst){
+        window.alert("Pontos insuficientes, necessita mais " + (midOptionConst-userPoints) + " pontos");
+        return;
+    }
+    if(confirm("Tem a certeza que pretende gastar " + midOptionConst + " pontos?")){
+        removePointsFromUser(cheapOptionConst);
+        window.location.href = "random_mistery.html";
+    }
+}
+
+function expensiveOption(){
+    if(userPoints<expensiveOptionConst){
+        window.alert("Pontos insuficientes, necessita mais " + (expensiveOptionConst-userPoints) + " pontos");
+        return;
+    }
+    if(confirm("Tem a certeza que pretende gastar " + expensiveOptionConst + " pontos?")){
+        removePointsFromUser(cheapOptionConst);
+        window.location.href = "random_mistery.html";
+    }
+}
+
+function removePointsFromUser(points){
+    var usersJSON = getFromStorage(usersConst);
+    for(var user of usersJSON.users)
+        if(user.id == logged.id)
+            user.points -= points;
+    loadToStorage(usersConst, usersJSON);
+}
