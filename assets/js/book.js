f4b21abf1fda7d114873f1e0a4f896b198e1beb5
nmfSantos <nmf.santos@campus.fct.unl.pt>
2020-11-27 16:08:20 +0000

    Added dynamic data to book

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,3 +1,17 @@
-var qs = new Querystring();
-var id = qs.get("id");
-console.log(id);
+var qs = new URLSearchParams(window.location.search);
+const isbn = qs.get('id');
+
+var books = getFromStorage(booksConst);
+var book;
+for(b of books.books){
+    if(b.isbn == isbn)
+        book = b
+}
+
+document.getElementById("book-isbn").innerHTML = book.isbn;
+document.getElementById("book-title").innerHTML = book.name;
+document.getElementById("book-author").innerHTML = book.author;
+document.getElementById("book-year").innerHTML = book.year;
+document.getElementById("book-genres").innerHTML = book.genre;
+// document.getElementById("book-synopsis").innerHTML = book.synopsis;
+
