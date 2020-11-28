67625a79597e0a9d22982c4b637a7821b51ac60e
Hugo Santiago <h.assuncao@campus.fct.unl.pt>
2020-11-28 18:26:45 +0000

    catalogo

--- assets/js/information.js
+++ assets/js/information.js
@@ -1,9 +1,9 @@
 var users = '{ "users" : [' +
     '{"id" : 1, "firstname" : "Hugo", "surname" : "Santiago", "email": "hugosantiago@gmail.com", "morada": "Rua Terceira, nº 17, 1º Esq., Telheiras", "points" : 100, "wishlist" : ["512278208", "681860960"], "available" : ["123456789", "987654321", "200738799"]},' +
-    '{"id" : 2, "firstname" : "Nuno", "surname" : "Santos", "email": "nunosantos@gmail.com", "morada": "Avenida do Travesso, nº 22, 4º Esq., Olhão", "points" : 100, "wishlist" : ["987654321", "512278208"], "available" : ["709026838"]},' +
-    '{"id" : 3, "firstname" : "João", "surname" : "Paulo", "email": "joaopaulo@gmail.com", "morada": "Praça António Vilelas, nº 2, 2º Dir., Caminha", "points" : 100, "wishlist" : ["987654321", "709026838"], "available" : ["123456789"]},' +
-    '{"id" : 4, "firstname" : "Pedro", "surname" : "Belinha", "email": "pedrobelinha@gmail.com", "morada": "Rua das Papoilas, nº 55, R/C Esq., Vila Nova de Milfontes", "points" : 100, "wishlist" : ["286061548", "512278208"], "available" : ["200738799", "512278208"]},' +
-    '{"id" : 5, "firstname" : "Ana", "surname" : "Catarina", "email": "anacatarina@gmail.com", "morada": "Travessa do Alto do Mouro, nº 34, 1º Dir., Fátima", "points" : 100, "wishlist" : ["200738799", "286061548"], "available" : ["709026838"]},' +
+    '{"id" : 2, "firstname" : "Nuno", "surname" : "Santos", "email": "nunosantos@gmail.com", "morada": "Avenida do Travesso, nº 22, 4º Esq., Olhão", "points" : 100, "wishlist" : ["987654321", "512278208", "934683429"], "available" : ["709026838"]},' +
+    '{"id" : 3, "firstname" : "João", "surname" : "Paulo", "email": "joaopaulo@gmail.com", "morada": "Praça António Vilelas, nº 2, 2º Dir., Caminha", "points" : 100, "wishlist" : ["987654321", "709026838"], "available" : ["123456789", "534321113"]},' +
+    '{"id" : 4, "firstname" : "Pedro", "surname" : "Belinha", "email": "pedrobelinha@gmail.com", "morada": "Rua das Papoilas, nº 55, R/C Esq., Vila Nova de Milfontes", "points" : 100, "wishlist" : ["286061548", "512278208"], "available" : ["200738799", "878633222", "666532112"]},' +
+    '{"id" : 5, "firstname" : "Ana", "surname" : "Catarina", "email": "anacatarina@gmail.com", "morada": "Travessa do Alto do Mouro, nº 34, 1º Dir., Fátima", "points" : 100, "wishlist" : ["200738799", "286061548"], "available" : ["709026838", "934683429"]},' +
     '{"id" : 6, "firstname" : "John", "surname" : "Hubbard", "email": "johnhubbard@gmail.com", "morada": "St. Dennis Street, 1224, Birmingham", "points" : 100, "wishlist" : ["709026838"], "available" : ["591171946", "512278208"]}' +
 ']}';
 
@@ -40,8 +40,12 @@ var catalog = '{ "catalog" : [' +
     '{ "isbn": "987654321", "ids": [1] },' +
     '{ "isbn": "200738799", "ids": [1,4] },' +
     '{ "isbn": "709026838", "ids": [2,5] },' +
-    '{ "isbn": "512278208", "ids": [4,6] },' +
-    '{ "isbn": "591171946", "ids": [6] }' +
+    '{ "isbn": "512278208", "ids": [6] },' +
+    '{ "isbn": "591171946", "ids": [6] },' +
+    '{ "isbn": "534321113", "ids": [3] },' +
+    '{ "isbn": "878633222", "ids": [4] },' +
+    '{ "isbn": "666532112", "ids": [4] },' +
+    '{ "isbn": "934683429", "ids": [5] }' +
 ']}';
 
 var authors = '{"authors" : [' +
